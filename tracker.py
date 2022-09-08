a2a297f2dd2edbf75238dc85fdd21d3e68a67e69
SayanthD <13906889+SayanthD@users.noreply.github.com>
2022-09-08 20:04:40 +0530

    Fix fetching new updates after site change.
    tracker.py: Use requests-html to render js
    regions.yml: Replace 'ae' with 'mea-en'

--- tracker.py
+++ tracker.py
@@ -7,12 +7,12 @@ from os import environ, system, rename, path
 
 import yaml
 from bs4 import BeautifulSoup
-from requests import get, post
+from requests_html import HTMLSession
 
 # Setup variables
 BOT_TOKEN = environ["realme_tg_bot_token"]
 CHAT = "@RealmeUpdatesTracker"
-GIT_OAUTH_TOKEN = environ['GIT_TOKEN']
+GIT_OAUTH_TOKEN = environ["GIT_TOKEN"]
 
 SITE = "https://realmeupdater.com"
 R_SITE = "https://realme.com"
@@ -28,21 +28,23 @@ def update_device(codename: str, device: str):
     :param device: device name
     """
     try:
-        if DEVICES[codename] and device not in DEVICES[codename].split('/'):
+        if DEVICES[codename] and device not in DEVICES[codename].split("/"):
             DEVICES.update({codename: f"{DEVICES[codename]}/{device}"})
     except KeyError:
         DEVICES.update({codename: device})
 
 
-def get_downloads_html(url: str) -> list:
+def get_downloads_html(session: HTMLSession, url: str) -> list:
     """
     Scrap downloads info from the website
     :param url: realme downloads page
     :return: list of devices latest downloads HTML
     """
-    downloads_html = BeautifulSoup(get(url).text, "html.parser") \
-        .select_one("div.software-items").select("div.software-item")
-    return downloads_html
+    response = session.get(url)
+    response.html.render(sleep=5)
+    soup = BeautifulSoup(response.html.html, "html.parser")
+    items = soup.select_one("div.software-items")
+    return items.select("div.software-item") if items is not None else []
 
 
 def clean_text(text: str) -> str:
@@ -51,7 +53,7 @@ def clean_text(text: str) -> str:
     :param text: string to clean
     :return: cleaned string
     """
-    return text.strip().replace('  ', ' ')
+    return text.strip().replace("  ", " ")
 
 
 def parse_html(html: list, region: str) -> list:
@@ -71,33 +73,40 @@ def parse_html(html: list, region: str) -> list:
         if not _system:
             continue
         try:
-            version = re.search(r'([A-Z0-9+]+_[0-9]+(?:.|_)[A-Z]+(?:.|_)[0-9]+)',
-                                item.select("div.software-field")[0].text).group(1)
-            codename = version.split('_')[0]
+            version = re.search(
+                r"([A-Z0-9+]+_[0-9]+(?:.|_)[A-Z]+(?:.|_)[0-9]+)",
+                item.select("div.software-field")[0].text,
+            ).group(1)
+            codename = version.split("_")[0]
         except (IndexError, AttributeError):
             version = "Unknown"
             codename = "Unknown"
         try:
-            date = item.select("div.software-field")[1].text.strip().split(": ")[1].strip()
-            if len(date.split('/')[0]) == 4:
+            date = (
+                item.select("div.software-field")[1].text.strip().split(": ")[1].strip()
+            )
+            if len(date.split("/")[0]) == 4:
                 date = datetime.strptime(date, "%Y/%m/%d").strftime("%d/%m/%Y")
         except IndexError:
             date = "Unknown"
         size = clean_text(item.select("div.software-field")[2].span.text)
-        if size.endswith('G'):
-            size = size.replace('G', 'GB')
+        if size.endswith("G"):
+            size = size.replace("G", "GB")
         try:
-            md5 = item.select("div.software-field")[3].text.strip().split(": ")[1].strip()
+            md5 = (
+                item.select("div.software-field")[3].text.strip().split(": ")[1].strip()
+            )
         except IndexError:
             md5 = "Unknown"
         download = item.select_one("div.software-download").select_one(
-            "a.software-button")["data-href"]
+            "a.software-button"
+        )["data-href"]
         if download == "https://download.c.realme.com/osupdate/":
             continue
         changelog = item.select_one("div.software-log").get_text("\n", strip=True)
         changelog_text = ""
         for line in changelog.splitlines():
-            if line.startswith('●') or line.startswith('*'):
+            if line.startswith("●") or line.startswith("*"):
                 changelog_text += f"{line}\n"
             else:
                 changelog_text += f"**{line}**:\n"
@@ -111,7 +120,7 @@ def parse_html(html: list, region: str) -> list:
             "size": size,
             "md5": md5,
             "download": download,
-            "changelog": changelog_text
+            "changelog": changelog_text,
         }
         if download:
             write_yaml(update, f"data/{region}/{codename}.yml")
@@ -131,12 +140,12 @@ def write_yaml(downloads, filename: str):
     def str_presenter(dumper, data):
         # https://stackoverflow.com/a/33300001
         if len(data.splitlines()) > 1:
-            return dumper.represent_scalar('tag:yaml.org,2002:str', data, style='|')
-        return dumper.represent_scalar('tag:yaml.org,2002:str', data)
+            return dumper.represent_scalar("tag:yaml.org,2002:str", data, style="|")
+        return dumper.represent_scalar("tag:yaml.org,2002:str", data)
 
     yaml.add_representer(str, str_presenter)
 
-    with open(f"{filename}", 'w') as out:
+    with open(f"{filename}", "w", encoding="UTF-8") as out:
         yaml.dump(downloads, out, allow_unicode=True)
 
 
@@ -147,11 +156,11 @@ def merge_yaml(regions: dict):
     yaml_files = [value for key, value in regions.items()]
     yaml_data = []
     for file in yaml_files:
-        with open(f"data/{file}/{file}.yml", "r") as yaml_file:
+        with open(f"data/{file}/{file}.yml", "r", encoding="UTF-8") as yaml_file:
             updates = yaml.load(yaml_file, Loader=yaml.FullLoader)
             for update in updates:
                 yaml_data.append(update)
-    with open('data/latest.yml', "w") as output:
+    with open("data/latest.yml", "w", encoding="UTF-8") as output:
         yaml.dump(yaml_data, output, allow_unicode=True)
 
 
@@ -159,13 +168,14 @@ def merge_archive():
     """
     merge all archive yaml files into one file
     """
-    yaml_files = [x for x in sorted(glob('data/archive/*.yml'))
-                  if not x.endswith('archive.yml')]
+    yaml_files = [
+        x for x in sorted(glob("data/archive/*.yml")) if not x.endswith("archive.yml")
+    ]
     yaml_data = []
     for file in yaml_files:
-        with open(file, "r") as yaml_file:
+        with open(file, "r", encoding="UTF-8") as yaml_file:
             yaml_data.append(yaml.load(yaml_file, Loader=yaml.FullLoader))
-    with open('data/archive/archive.yml', "w") as output:
+    with open("data/archive/archive.yml", "w", encoding="UTF-8") as output:
         yaml.dump(yaml_data, output, allow_unicode=True)
 
 
@@ -176,8 +186,11 @@ def diff_yaml(filename: str) -> list:
     :return: list of dictionaries of new updates
     """
     try:
-        with open(f'data/{filename}/{filename}.yml', 'r') as new, \
-                open(f'data/{filename}/old_{filename}', 'r') as old_data:
+        with open(
+            f"data/{filename}/{filename}.yml", "r", encoding="UTF-8"
+        ) as new, open(
+            f"data/{filename}/old_{filename}", "r", encoding="UTF-8"
+        ) as old_data:
             latest = yaml.load(new, Loader=yaml.FullLoader)
             old = yaml.load(old_data, Loader=yaml.FullLoader)
             first_run = False
@@ -186,14 +199,18 @@ def diff_yaml(filename: str) -> list:
         first_run = True
     if first_run is False:
         if len(latest) == len(old):
-            return [new_ for new_, old_ in zip(latest, old)
-                    if not new_['version'] == old_['version']]
+            return [
+                new_
+                for new_, old_ in zip(latest, old)
+                if not new_["version"] == old_["version"]
+            ]
         old_codenames = [i["codename"] for i in old]
         new_codenames = [i["codename"] for i in latest]
         changes = [i for i in new_codenames if i not in old_codenames]
         if changes:
-            return [i for i in latest for codename in changes
-                    if codename == i["codename"]]
+            return [
+                i for i in latest for codename in changes if codename == i["codename"]
+            ]
 
 
 def generate_message(update: dict) -> str:
@@ -212,35 +229,37 @@ def generate_message(update: dict) -> str:
     download = update["download"]
     changelog = update["changelog"]
     message = "New update available!\n"
-    message += f"*Device:* {device} \n" \
-               f"*Codename:* #{codename} \n" \
-               f"*Region:* [{region}]({SITE}/downloads/latest/{region})\n" \
-               f"*System:* {_system} \n" \
-               f"*Version:* `{version}` \n" \
-               f"*Release Date:* {date} \n" \
-               f"*Size*: {size} \n" \
-               f"*MD5*: `{md5}`\n" \
-               f"*Download*: [Here]({download})\n" \
-               f"*Changelog*: ```\n{changelog}\n```\n" \
-               f"[Latest Updates]({SITE}/downloads/latest/{codename}/) - " \
-               f"[All Updates]({SITE}/downloads/archive/{codename}/)\n" \
-               "@RealmeUpdatesTracker"
+    message += (
+        f"*Device:* {device} \n"
+        f"*Codename:* #{codename} \n"
+        f"*Region:* [{region}]({SITE}/downloads/latest/{region})\n"
+        f"*System:* {_system} \n"
+        f"*Version:* `{version}` \n"
+        f"*Release Date:* {date} \n"
+        f"*Size*: {size} \n"
+        f"*MD5*: `{md5}`\n"
+        f"*Download*: [Here]({download})\n"
+        f"*Changelog*: ```\n{changelog}\n```\n"
+        f"[Latest Updates]({SITE}/downloads/latest/{codename}/) - "
+        f"[All Updates]({SITE}/downloads/archive/{codename}/)\n"
+        "@RealmeUpdatesTracker"
+    )
     return message
 
 
-def tg_post(message: str) -> int:
+def tg_post(session: HTMLSession, message: str) -> int:
     """
     post message to telegram
     :return: post request status code
     """
     params = (
-        ('chat_id', CHAT),
-        ('text', message),
-        ('parse_mode', "Markdown"),
-        ('disable_web_page_preview', "yes")
+        ("chat_id", CHAT),
+        ("text", message),
+        ("parse_mode", "Markdown"),
+        ("disable_web_page_preview", "yes"),
     )
-    telegram_url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage"
-    telegram_req = post(telegram_url, params=params)
+    telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    telegram_req = session.post(telegram_url, params=params)
     telegram_status = telegram_req.status_code
     if telegram_status == 200:
         pass
@@ -256,20 +275,23 @@ def tg_post(message: str) -> int:
 
 def archive(update: dict):
     """Append new update to the archive"""
-    link = update['download']
-    version = update['version']
-    codename = link.split('/')[-1].split('_')[0] \
-        if 'sign' not in link else link.split('/')[-1].split('_')[1]
+    link = update["download"]
+    version = update["version"]
+    codename = (
+        link.split("/")[-1].split("_")[0]
+        if "sign" not in link
+        else link.split("/")[-1].split("_")[1]
+    )
     try:
-        with open(f'data/archive/{codename}.yml', 'r') as yaml_file:
+        with open(f"data/archive/{codename}.yml", "r", encoding="UTF-8") as yaml_file:
             data = yaml.load(yaml_file, Loader=yaml.FullLoader)
             data[codename].update({version: link})
             data.update({codename: data[codename]})
-            with open(f'data/archive/{codename}.yml', 'w') as output:
+            with open(f"data/archive/{codename}.yml", "w", encoding="UTF-8") as output:
                 yaml.dump(data, output, allow_unicode=True)
     except FileNotFoundError:
         data = {codename: {version: link}}
-        with open(f'data/archive/{codename}.yml', 'w') as output:
+        with open(f"data/archive/{codename}.yml", "w", encoding="UTF-8") as output:
             yaml.dump(data, output, allow_unicode=True)
 
 
@@ -277,25 +299,29 @@ def git_commit_push():
     """
     git add - git commit - git push
     """
-    today = str(datetime.today()).split('.')[0]
-    system("git add *.yml */*.yml && git -c \"user.name=RealmeCI\" -c "
-           "\"user.email=RealmeCI@example.com\" "
-           "commit -m \"sync: {}\" && "" \
-           ""git push -q https://{}@github.com/RealmeUpdater/"
-           "realme-updates-tracker.git HEAD:master"
-           .format(today, GIT_OAUTH_TOKEN))
+    today = str(datetime.today()).split(".")[0]
+    system(
+        'git add *.yml */*.yml && git -c "user.name=RealmeCI" -c '
+        '"user.email=RealmeCI@example.com" '
+        'commit -m "sync: {}" && '
+        " \
+           "
+        "git push -q https://{}@github.com/RealmeUpdater/"
+        "realme-updates-tracker.git HEAD:master".format(today, GIT_OAUTH_TOKEN)
+    )
 
 
 def main():
     """
     Realme updates scraper and tracker
     """
-    with open("data/regions.yml", "r") as yaml_file:
+    session = HTMLSession()
+    with open("data/regions.yml", "r", encoding="UTF-8") as yaml_file:
         regions = yaml.load(yaml_file, Loader=yaml.FullLoader)
     for region_code, region in regions.items():
-        if path.exists(f'data/{region}/{region}.yml'):
-            rename(f'data/{region}/{region}.yml', f'data/{region}/old_{region}')
-        downloads_html = get_downloads_html(f"{R_SITE}/{region_code}/{PAGE}")
+        if path.exists(f"data/{region}/{region}.yml"):
+            rename(f"data/{region}/{region}.yml", f"data/{region}/old_{region}")
+        downloads_html = get_downloads_html(session, f"{R_SITE}/{region_code}/{PAGE}")
         updates = parse_html(downloads_html, region)
         write_yaml(updates, f"data/{region}/{region}.yml")
     merge_yaml(regions)
@@ -307,16 +333,17 @@ def main():
                     continue
                 message = generate_message(update)
                 # print(message)
-                status = tg_post(message)
+                status = tg_post(session, message)
                 if status == 200:
                     print(f"{update['device']}: Telegram Message sent successfully")
                 archive(update)
         else:
             print(f"{region}: No new updates.")
+    session.close()
     merge_archive()
     write_yaml(DEVICES, "data/devices.yml")
     git_commit_push()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
